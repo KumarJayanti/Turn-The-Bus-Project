e0f4161e2a57aba9dd4673c2668a50dc82666f47
S.R. <66353809+DEVELOPERX-coder@users.noreply.github.com>
2024-10-18 19:35:40 +0530

    reupdated after crash fetch

--- Assets/Script/VisualComponents/VoltmeterText.cs
+++ Assets/Script/VisualComponents/VoltmeterText.cs
@@ -9,12 +9,7 @@ public class VoltmeterText : MonoBehaviour
 
     public void UpdateVoltageValue(double voltage)
     {
-        // Debug.Log(voltage);
-        if(voltage >=100 || voltage <= -100) 
-        { 
-            
-            VoltageValue.text = string.Format("{0:0.##}", voltage/1000) + " V"; 
-        }
+        if(voltage >=100 || voltage <= -100) { VoltageValue.text = string.Format("{0:0.##}", voltage/1000) + " V"; }
         else { VoltageValue.text = string.Format("{0:0.##}", voltage) + " mV"; }
     }
 
