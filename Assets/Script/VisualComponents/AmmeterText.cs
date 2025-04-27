f552e09085355048dd026d46990c3b41b5fcea39
S.R. <66353809+DEVELOPERX-coder@users.noreply.github.com>
2025-04-27 21:08:36 +0530

    Value Calibration (Electronic Labs)

--- Assets/Script/VisualComponents/AmmeterText.cs
+++ Assets/Script/VisualComponents/AmmeterText.cs
@@ -9,17 +9,23 @@ public class AmmeterText : MonoBehaviour
 
     public void UpdateAmmeterValue(double ammeterValue)
     {
+        System.Random random = new System.Random();
+        float minValue = -0.05f;
+        float maxValue = 0.05f;
+
+        float randomFloat = (float)(random.NextDouble() * (maxValue - minValue) + minValue);
+
         if (ammeterValue >= 100 || ammeterValue <= -100)
         {
-            AmmeterValue.text = string.Format("{0:0.##}", ammeterValue / 1000) + " A";
+            AmmeterValue.text = string.Format("{0:0.##}", (ammeterValue / 1000) + randomFloat * (ammeterValue / 1000)) + " A";
         }
         else if (ammeterValue > 0.001 || ammeterValue < -0.001)
         {
-            AmmeterValue.text = string.Format("{0:0.##}", ammeterValue) + " mA";
+            AmmeterValue.text = string.Format("{0:0.##}", (ammeterValue) + randomFloat * (ammeterValue)) + " mA";
         }
         else
         {
-            AmmeterValue.text = string.Format("{0:0.##}", ammeterValue * 1e3) + " \u03bcA";
+            AmmeterValue.text = string.Format("{0:0.##}", (ammeterValue * 1e3) + randomFloat * (ammeterValue * 1e3)) + " \u03bcA";
         }
     }
 
